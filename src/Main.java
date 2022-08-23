98bdd68d646dde8fb4ce59a9fe7b723eb9ae22fd
user <amin7061205@gmail.com>
2022-08-23 17:45:51 +0300

    високосный год

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int total = 0;
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            total++;
+            System.out.println("Введите год и количество дней в формате yyyy число");
+            int year = scanner.nextInt();
+            System.out.println("Вы ввели:" + " " + year + " " + "год");
+            int day = scanner.nextInt();
+            System.out.println("Вы ввели количество дней равное:" + " " + day + " " + "день");
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0 && day == 366 || day == 365) {
+                System.out.println("Ответ правильный!!!");
+            } else {
+                System.out.println("Ответ Неправильный!!!");
+                System.out.println("Набрано очков:" + " " + total);
+                break;
+
+            }
+        }
+
+    }
+
+}
